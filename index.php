d03042cd1e4e973cff03f45a78cd5c1f1859dbaa
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-03-21 00:06:28 +0000

    update SVG fill and stroke colors in index.php for consistency

--- index.php
+++ index.php
@@ -330,16 +330,16 @@
                   <path
                     opacity="0.2"
                     d="M32 56C45.2548 56 56 45.2548 56 32C56 18.7452 45.2548 8 32 8C18.7452 8 8 18.7452 8 32C8 45.2548 18.7452 56 32 56Z"
-                    fill="currentColor" />
+                    fill="#000000" />
                   <path
                     d="M32 56C45.2548 56 56 45.2548 56 32C56 18.7452 45.2548 8 32 8C18.7452 8 8 18.7452 8 32C8 45.2548 18.7452 56 32 56Z"
-                    stroke="currentColor"
+                    stroke="#000000"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round" />
                   <path
                     d="M32 20V32L40 40"
-                    stroke="currentColor"
+                    stroke="#000000"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round" />
