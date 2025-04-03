7fe95e21fc00a9117bb14beaed95db0d15eea1b8
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-04-03 13:18:43 +0000

    fix: add line break for improved layout consistency in dashboard

--- dashboard.php
+++ dashboard.php
@@ -192,7 +192,7 @@ try {
                 <?php include 'includes/header.php'; ?>
                 <br>
                 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4"><br>
                     <!-- Page Header -->
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2"></h1>
