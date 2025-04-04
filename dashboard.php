4dc62254f5b2af09c04d19aef09329b5ac704530
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-04-04 10:58:39 +0000

    fix: remove unnecessary whitespace and add empty page header in dashboard

--- dashboard.php
+++ dashboard.php
@@ -182,7 +182,6 @@ try {
             font-weight: bold;
             color: #6c757d;
         }
-        
     </style>
 </head>
 <body>
@@ -196,6 +195,7 @@ try {
                 <main class="col-md-9 ms-sm-auto col-lg-14 px-md-4 py-4"><br>
                     <!-- Page Header -->
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        <h1 class="h2"></h1>
                         <div class="btn-toolbar mb-2 mb-md-0">
                             <div class="btn-group me-2">
                                 <button type="button" class="btn btn-sm btn-outline-secondary"> <i class="bi bi-share action-icon icon"></i>&nbsp;Share</button>
