4bfc0fc465bcba96728217f5ccaa8e74e65a8265
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-04-05 00:36:45 +0000

    fix: simplify page title in reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -14,7 +14,7 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Demo: Reset Password Basic - Pages | Sneat - Bootstrap Dashboard PRO</title>
+    <title>Reset Password</title>
 
     <meta name="description" content="" />
 
