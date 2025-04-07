df5cb5d08f6a19cfdb21093037b8adfc3c3b1657
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-04-07 17:48:17 +0000

    fix: update require paths for auth_check and dbconnection to use relative paths

--- election.php
+++ election.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../../includes/auth_check.php';
-require_once '../../configs/dbconnection.php';
+require_once 'includes/auth_check.php';
+require_once 'configs/dbconnection.php';
 
 $action = $_GET['action'] ?? 'manage';
 $electionID = $_GET['id'] ?? null;
