bfe57beb9fada12ee99a3162994915d49af308cf
Obuobi Ayim David <102563270+Aristocratjnr@users.noreply.github.com>
2025-07-06 20:34:16 +0000

    feat: Add favicon link to activity and security pages

--- activity.php
+++ activity.php
@@ -737,6 +737,7 @@ include 'includes/header.php';
     <div class="row">
         <!-- Sidebar -->
         <?php include 'includes/sidebar.php'; ?>
+         <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
         
         <!-- Main Content -->
         <main class="col-md-10 ms-auto col-lg-9 px-md-6 py-3"><br><br><br>
